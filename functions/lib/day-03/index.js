19ea815c09a61fbdb9661928b66b87c1766f4828
Dave Follett <davefollett@gmail.com>
2024-12-03 14:56:49 -0500

    might have the files working for day 03.

--- functions/lib/day-03/index.js
+++ functions/lib/day-03/index.js
@@ -115,24 +115,27 @@ export function part2(inputStr) {
 
 export function run() {
   const results = new Result('Day 03 - <i class="nes-icon is-medium star"></i><i class="nes-icon is-medium star"></i>');
-  const inputFilename = './functions/lib/day-03/input.txt';
+  // const inputFilename = './functions/lib/day-03/input.txt';
+  const inputFilename = path.resolve(__dirname + '/lib/day-03/input.txt');
 
   console.log(path.resolve(__dirname));
 
   let start = performance.now();
   // results.part1.answer = part1(input);
-  results.part1.answer = path.resolve(__dirname);//part1FromFile(inputFilename);
+  // results.part1.answer = path.resolve(__dirname);
+  results.part1.answer = part1FromFile(inputFilename);
   results.part1.time = (performance.now() - start).toFixed(2);
 
-  let fileList = ""
-  fs.readdirSync(path.resolve(__dirname + "/lib/day-03/")).forEach(file => {
-    fileList += ` ${file}`
-  });
+  // let fileList = ""
+  // fs.readdirSync(path.resolve(__dirname + "/lib/day-03/")).forEach(file => {
+  //   fileList += ` ${file}`
+  // });
 
 
   start = performance.now();
   // results.part2.answer = part2(input);
-  results.part2.answer = fileList;//part2FromFile(inputFilename);
+  // results.part2.answer = fileList;
+  results.part2.answer = part2FromFile(inputFilename);
   results.part2.time = (performance.now() - start).toFixed(2);
 
   return results;

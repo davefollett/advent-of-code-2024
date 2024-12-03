fbfd4150b13cc2bc1fcd6c7d1558fca8eb546e0e
Dave Follett <davefollett@gmail.com>
2024-12-03 14:18:22 -0500

    Day 03, parts 1 and 2 working.  Lots to clean up, hopefully the file reading works when deployed on netlify.

--- functions/lib/day-03/index.js
+++ functions/lib/day-03/index.js
@@ -4,34 +4,125 @@ import { performance } from 'node:perf_hooks';
 import { input } from '#lib/day-03/inputs.js';
 import Result from '#utils/result.js';
 import { inputParser } from '#lib/utils/input-parser.js';
+import fileParser from '#lib/utils/file-parser.js';
+import { sumNumbers } from '#utils/array.js';
+
+let mode = 'do';
 
 function lineParser1(line) {
-  return line;
+  const regexp = /mul\([0-9]*,[0-9]*\)/g;
+  const match = line.match(regexp);
+
+  const result = match.reduce((accu, item) => {
+    const left = item.split(',')[0].slice('mul('.length);
+    const right = item.split(',')[1].slice(0, -1);
+    accu += left * right;
+    return accu;
+  }, 0);
+
+  return result;
+}
+
+
+
+export function part1FromFile(filename) {
+  const lines = fileParser(filename, lineParser1);
+  console.log(lines)
+  return sumNumbers(lines);
+}
+
+
+export function part1(inputStr) {
+  const regexp = /mul\([0-9]*,[0-9]*\)/g;
+  const match = inputStr.match(regexp);
+  // console.log(match)
+
+  const result = match.reduce((accu, item) => {
+    const left = item.split(',')[0].slice('mul('.length);
+    const right = item.split(',')[1].slice(0, -1);
+    accu += left * right;
+    return accu;
+  }, 0);
+
+  return result;
+}
+
+function compute(arr) {
+  const result = arr.reduce((accu, item) => {
+    const left = item.split(',')[0].slice('mul('.length);
+    const right = item.split(',')[1].slice(0, -1);
+    accu += left * right;
+    return accu;
+  }, 0);
+
+  return result;
 }
 
 function lineParser2(line) {
-  return line;
+  const regexp = /mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\)/g;
+  const match = line.match(regexp);
+  // console.log(match)
+
+  const arr = match.filter((item) => {
+    if (item.startsWith('do(')) {
+      mode = 'do';
+      return false;
+    }
+    if (item.startsWith('don')) {
+      mode = "dont";
+      return false;
+    }
+    if (item.startsWith('mul') && mode === 'do') {
+      return true;
+    }
+    return false;
+  });
+
+  return compute(arr);
 }
 
-export function part1(inputStr) {
-  const lines = inputParser(inputStr, lineParser1);
-  return lines.length;
+export function part2FromFile(filename) {
+  const lines = fileParser(filename, lineParser2);
+  return sumNumbers(lines);
 }
 
 export function part2(inputStr) {
-  const lines = inputParser(inputStr, lineParser2);
-  return  lines.length;
+  const regexp = /mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\)/g;
+  const match = inputStr.match(regexp);
+  // console.log(match)
+
+  let mode = 'do';
+
+  const arr = match.filter((item) => {
+    if (item.startsWith('do(')) {
+      mode = 'do';
+      return false;
+    }
+    if (item.startsWith('don')) {
+      mode = "dont";
+      return false;
+    }
+    if (item.startsWith('mul') && mode === 'do') {
+      return true;
+    }
+    return false;
+  });
+
+  return compute(arr);//result;
 }
 
 export function run() {
   const results = new Result('Day 03 - <i class="nes-icon is-medium star"></i><i class="nes-icon is-medium star"></i>');
+  const inputFilename = './functions/lib/day-03/input.txt';
 
   let start = performance.now();
-  results.part1.answer = part1(input);
+  // results.part1.answer = part1(input);
+  results.part1.answer = part1FromFile(inputFilename);
   results.part1.time = (performance.now() - start).toFixed(2);
 
   start = performance.now();
-  results.part2.answer = part2(input);
+  // results.part2.answer = part2(input);
+  results.part2.answer = part2FromFile(inputFilename);
   results.part2.time = (performance.now() - start).toFixed(2);
 
   return results;
--- functions/lib/day-03/index.test.js
+++ functions/lib/day-03/index.test.js
@@ -1,29 +1,33 @@
 import { describe, expect, test } from 'vitest';
-import { part1, part2 } from '#lib/day-03/index.js';
-import { input, testInput } from '#lib/day-03/inputs.js';
+import { part1, part1FromFile, part2, part2FromFile } from '#lib/day-03/index.js';
+import { input, testInput, testInput2 } from '#lib/day-03/inputs.js';
+
+const inputFilename = './functions/lib/day-03/input.txt';
 
 describe('@/day-03/index.js', () => {
   describe('part1()', () => {
     test.skip('part1 should pass', () => {
-      const answer = part1(input);
-      expect(answer).toBe(0);
+      const answer = part1FromFile(inputFilename);
+      expect(answer).toBe(173419328);
     });
 
-    test.skip('part1 should pass test input', () => {
+    test('part1 should pass test input', () => {
+      // console.log(input[0])
       const answer = part1(testInput);
-      expect(answer).toBe(0);
+      expect(answer).toBe(161);
     });
   });
 
   describe('part2()', () => {
-    test.skip('part2 should pass', () => {
-      const answer = part2(input);
-      expect(answer).toBe(0);
+    test('part2 should pass', () => {
+      // to high: 91155532
+      const answer = part2FromFile(inputFilename);
+      expect(answer).toBe(90669332);
     });
 
-    test.skip('part2 should pass test input', () => {
-      const answer = part2(testInput);
-      expect(answer).toBe(0);
+    test('part2 should pass test input', () => {
+      const answer = part2(testInput2);
+      expect(answer).toBe(48);
     });
   });
 });

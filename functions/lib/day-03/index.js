3f68618a75babaeaf6eaa9fa449a0c04cb97c5ae
Dave Follett <davefollett@gmail.com>
2024-12-04 22:51:30 -0500

    2024 Day 03 cleanup.

--- functions/lib/day-03/index.js
+++ functions/lib/day-03/index.js
@@ -1,11 +1,7 @@
 // https://adventofcode.com/2024/day/3
-import path from 'path';
-import fs from 'fs';
 
 import { performance } from 'node:perf_hooks';
-import { input } from '#lib/day-03/inputs.js';
 import Result from '#utils/result.js';
-import { inputParser } from '#lib/utils/input-parser.js';
 import fileParser from '#lib/utils/file-parser.js';
 import { sumNumbers } from '#utils/array.js';
 
@@ -25,28 +21,10 @@ function lineParser1(line) {
   return result;
 }
 
-
-
-export function part1FromFile(filename) {
+export function part1(filename) {
   const lines = fileParser(filename, lineParser1);
   console.log(lines)
   return sumNumbers(lines);
-}
-
-
-export function part1(inputStr) {
-  const regexp = /mul\([0-9]*,[0-9]*\)/g;
-  const match = inputStr.match(regexp);
-  // console.log(match)
-
-  const result = match.reduce((accu, item) => {
-    const left = item.split(',')[0].slice('mul('.length);
-    const right = item.split(',')[1].slice(0, -1);
-    accu += left * right;
-    return accu;
-  }, 0);
-
-  return result;
 }
 
 function compute(arr) {
@@ -63,7 +41,6 @@ function compute(arr) {
 function lineParser2(line) {
   const regexp = /mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\)/g;
   const match = line.match(regexp);
-  // console.log(match)
 
   const arr = match.filter((item) => {
     if (item.startsWith('do(')) {
@@ -83,59 +60,23 @@ function lineParser2(line) {
   return compute(arr);
 }
 
-export function part2FromFile(filename) {
+export function part2(filename) {
+  mode = 'do';
   const lines = fileParser(filename, lineParser2);
+  mode = 'do';
   return sumNumbers(lines);
-}
-
-export function part2(inputStr) {
-  const regexp = /mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\)/g;
-  const match = inputStr.match(regexp);
-  // console.log(match)
-
-  let mode = 'do';
-
-  const arr = match.filter((item) => {
-    if (item.startsWith('do(')) {
-      mode = 'do';
-      return false;
-    }
-    if (item.startsWith('don')) {
-      mode = "dont";
-      return false;
-    }
-    if (item.startsWith('mul') && mode === 'do') {
-      return true;
-    }
-    return false;
-  });
-
-  return compute(arr);//result;
 }
 
 export function run() {
   const results = new Result('Day 03 - <i class="nes-icon is-medium star"></i><i class="nes-icon is-medium star"></i>');
-  // const inputFilename = './functions/lib/day-03/input.txt';
-  const inputFilename = path.resolve(__dirname + '/lib/day-03/input.txt');
-
-  console.log(path.resolve(__dirname));
+  const inputFilename = __dirname + '/lib/day-03/input.txt';
 
   let start = performance.now();
-  // results.part1.answer = part1(input);
-  // results.part1.answer = path.resolve(__dirname);
-  results.part1.answer = part1FromFile(inputFilename);
+  results.part1.answer = part1(inputFilename);
   results.part1.time = (performance.now() - start).toFixed(2);
 
-  // let fileList = ""
-  // fs.readdirSync(path.resolve(__dirname + "/lib/day-03/")).forEach(file => {
-  //   fileList += ` ${file}`
-  // });
-
-
   start = performance.now();
-  // results.part2.answer = part2(input);
-  // results.part2.answer = fileList;
-  results.part2.answer = part2FromFile(inputFilename);
+  results.part2.answer = part2(inputFilename);
   results.part2.time = (performance.now() - start).toFixed(2);
 
   return results;
--- functions/lib/day-03/index.test.js
+++ functions/lib/day-03/index.test.js
@@ -1,19 +1,19 @@
 import { describe, expect, test } from 'vitest';
-import { part1, part1FromFile, part2, part2FromFile } from '#lib/day-03/index.js';
-import { input, testInput, testInput2 } from '#lib/day-03/inputs.js';
+import { part1, part2 } from '#lib/day-03/index.js';
 
-const inputFilename = './functions/lib/day-03/input.txt';
+const inputFilename = __dirname + '/input.txt';
+const inputTest1Filename = __dirname + '/input-test-1.txt';
+const inputTest2Filename = __dirname + '/input-test-2.txt';
 
 describe('@/day-03/index.js', () => {
   describe('part1()', () => {
-    test.skip('part1 should pass', () => {
-      const answer = part1FromFile(inputFilename);
+    test('part1 should pass', () => {
+      const answer = part1(inputFilename);
       expect(answer).toBe(173419328);
     });
 
     test('part1 should pass test input', () => {
-      // console.log(input[0])
-      const answer = part1(testInput);
+      const answer = part1(inputTest1Filename);
       expect(answer).toBe(161);
     });
   });
@@ -21,12 +21,12 @@ describe('@/day-03/index.js', () => {
   describe('part2()', () => {
     test('part2 should pass', () => {
       // to high: 91155532
-      const answer = part2FromFile(inputFilename);
+      const answer = part2(inputFilename);
       expect(answer).toBe(90669332);
     });
 
     test('part2 should pass test input', () => {
-      const answer = part2(testInput2);
+      const answer = part2(inputTest2Filename);
       expect(answer).toBe(48);
     });
   });

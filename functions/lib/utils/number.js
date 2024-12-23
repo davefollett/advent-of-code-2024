dc93086698570a945a45c2e6d893858c80239d97
Dave Follett <davefollett@gmail.com>
2024-12-22 21:27:34 -0500

    Fixed formatting of the splitDigits() function comment.

--- functions/lib/utils/number.js
+++ functions/lib/utils/number.js
@@ -15,11 +15,11 @@ export function isOdd(num) {
  * number must have an even number of digits, otherwise { left: null, right: null } is returned.
  * 
  * Examples:
- *   123456 -> { left: 123, right: 456 }
- *   12 -> { left: 1, right: 2 }
- *   4209 -> { left: 42, right: 9 } // leading zeros round off because a number is returned
- *   7800 -> { left: 78, right: 0 } // leading zeros round off because a number is returned
- *   12345 -> { left: null, right: null } // provided number must have an even number of digits
+ *  - 123456 -> { left: 123, right: 456 }
+ *  - 12 -> { left: 1, right: 2 }
+ *  - 4209 -> { left: 42, right: 9 } // leading zeros round off because a number is returned
+ *  - 7800 -> { left: 78, right: 0 } // leading zeros round off because a number is returned
+ *  - 12345 -> { left: null, right: null } // provided number must have an even number of digits
  * 
  * @param {Number} num The number to split in half.
  * @returns An object with left and right keys.
